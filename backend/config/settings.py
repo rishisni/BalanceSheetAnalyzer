759e193e046c80519a9ed5f162ff52be9a2e7095
rishisni <rishabhkirodiwal04@gmail.com>
2025-11-03 01:58:22 +0530

    Added Aloowed Host

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -32,8 +32,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOWED_ORIGINS = ['*']
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://balance-sheet-analyzer-silk.vercel.app"
+]
 
 
 # Application definition
